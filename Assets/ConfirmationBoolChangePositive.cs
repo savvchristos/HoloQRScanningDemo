4e2759396c9e19b3640058b15dfd2f03e390f46c
Christos <savvchristos@gmail.com>
2024-05-14 17:10:10 +0300

    fixed UI, changed icons to white, broke the confirmation dialog and didn't load data from QR

--- Assets/ConfirmationBoolChangePositive.cs
+++ Assets/ConfirmationBoolChangePositive.cs
@@ -12,7 +12,7 @@ public class ConfirmationBoolChangePositive : MonoBehaviour
     private GameObject SupremeS500;
 
     [SerializeField]
-    private GameObject SupremeF85;
+    private GameObject SupremeSF85;
 
     public void ChangeBoolToTrue()
     {
@@ -21,14 +21,22 @@ public class ConfirmationBoolChangePositive : MonoBehaviour
         {
             //enable smartia ui
             SmartiaS67.SetActive(true);
+            SupremeS500.SetActive(false);
+            SupremeSF85.SetActive(false);
         }
         else if (gameobjectWithBool.GetComponent<QRCodeDisplayController>().lastSeenCode.Data == "https://SUPREMES500.GR")
         {
             //enable SUPREMES500 ui
+            SupremeS500.SetActive(true);
+            SmartiaS67.SetActive(false);
+            SupremeSF85.SetActive(false);
         }
         else if (gameobjectWithBool.GetComponent<QRCodeDisplayController>().lastSeenCode.Data == "https://SUPREMEF85.GR")
         {
-            //enable SUPREMEF85 ui
+            //enable SUPREMESF85 ui
+            SupremeS500.SetActive(false);
+            SmartiaS67.SetActive(false);
+            SupremeSF85.SetActive(true);
         }
     }
 }
